dbf461fef02cd0ac4661f6dd18e0b4cfc77f4e3c
Fe-56 <limfuoen1999@gmail.com>
2024-03-23 00:32:18 +0800

    Complete PMEmo feature extraction and data preproccessing

--- utils/paths.py
+++ utils/paths.py
@@ -2,3 +2,8 @@ deam_path = '../../data/DEAM'
 
 def get_deam_path(path):
   return f'{deam_path}/{path}'
+
+pmemo_path = '../../data/PMEmo/PMEmo2019'
+
+def get_pmemo_path(path):
+  return f'{pmemo_path}/{path}'
