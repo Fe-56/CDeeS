a1b736f78db9547d611c5669e8b8353618420a6c
issacj-17 <68261814+issacj-17@users.noreply.github.com>
2024-04-07 18:26:50 +0800

    Update feedforward_nn_combined.py
    - Renamed num_of_units to hidden_size
    - Added Dropout layers whose probability can be controlled during initialisation

--- research/models/feedforward_nn_combined.py
+++ research/models/feedforward_nn_combined.py
@@ -3,21 +3,29 @@ import torch.nn as nn
 import math
 
 class NeuralNetworkCombined(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self, input_size, output_size=2, dropout_prob=0.0):
         super(NeuralNetworkCombined, self).__init__()
-        self.num_of_units = math.ceil((input_size**0.5) * 1.5)
+
+        self.hidden_size = math.ceil((input_size**0.5) * 1.5)
+        self.p = dropout_prob
+
         self.layers = nn.Sequential(
-            nn.Linear(input_size, self.num_of_units),
+            nn.Linear(input_size, self.hidden_size),
             nn.ReLU(),
-            nn.Linear(self.num_of_units, self.num_of_units),
+            nn.Dropout(self.p),
+            nn.Linear(self.hidden_size, self.hidden_size),
             nn.ReLU(),
-            nn.Linear(self.num_of_units, self.num_of_units),
+            nn.Dropout(self.p),
+            nn.Linear(self.hidden_size, self.hidden_size),
             nn.ReLU(),
-            nn.Linear(self.num_of_units, self.num_of_units),
+            nn.Dropout(self.p),
+            nn.Linear(self.hidden_size, self.hidden_size),
             nn.ReLU(),
-            nn.Linear(self.num_of_units, self.num_of_units),
+            nn.Dropout(self.p),
+            nn.Linear(self.hidden_size, self.hidden_size),
             nn.ReLU(),
-            nn.Linear(self.num_of_units, 2)
+            nn.Dropout(self.p),
+            nn.Linear(self.hidden_size, output_size)
         )
 
     def forward(self, x):
